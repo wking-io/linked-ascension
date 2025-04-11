e88875410034eb73355f383579a2b49708fbbfa9
Will King <contact@wking.dev>
2025-04-11 14:02:05 -0500

    Please

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
-import { run } from 'vite-plugin-run';
 
 export default defineConfig({
     plugins: [
-        run([
-            {
-                name: 'wayfinder',
-                run: ['php', 'artisan', 'wayfinder:generate'],
-                pattern: ['routes/**/*.php', 'app/**/Http/**/*.php'],
-            },
-        ]),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
