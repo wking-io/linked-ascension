ce9a3eeec88f0be177090bbf1dfc7d76a71fb546
Will King <contact@wking.dev>
2025-04-11 20:25:05 -0500

    Fixing continue button bug

--- resources/js/pages/characters/welcome.tsx
+++ resources/js/pages/characters/welcome.tsx
@@ -148,13 +148,13 @@ export default function Welcome({ game_id, character_id }: Props) {
                 {showContinue && <DialogueArrow className="absolute -bottom-1 left-1/2 -translate-x-1/2 animate-bounce" />}
             </div>
             {isLastLine ? (
-                <ActionButton onClick={handleContinue} className="w-full">
-                    Continue
-                </ActionButton>
-            ) : (
                 <ActionLink href={claim([game_id, character_id])} className="w-full">
                     Claim Character
                 </ActionLink>
+            ) : (
+                <ActionButton onClick={handleContinue} className="w-full">
+                    Continue
+                </ActionButton>
             )}
         </div>
     );
